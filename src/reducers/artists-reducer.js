56d1bfdd3f3d3634e3db6819c641b6932b941d45
ari-rosie <62956748+ari-rosie@users.noreply.github.com>
2020-08-06 16:11:27 -0400

    Exercice-4

---
+++ src/reducers/artists-reducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+    artist: 'artist'
+  };
+  
+  export default function authReducer(state = initialState, action) {
+    switch (action.type) {
+      default: {
+        return state;
+      }
+    }
+  }
